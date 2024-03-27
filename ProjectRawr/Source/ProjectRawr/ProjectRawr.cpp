ef7a111a93f326b21aa974825bf867e3cad4020c
Maxime <32262840+duflosky@users.noreply.github.com>
2024-03-27 11:33:50 +0100

    feat: add unreal;

---
+++ ProjectRawr/Source/ProjectRawr/ProjectRawr.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectRawr.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectRawr, "ProjectRawr" );
+ 
---
+++ ProjectRawr/Source/ProjectRawr/ProjectRawrGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRawrGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectRawrGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectRawrGameMode();
+};
+
+
+
